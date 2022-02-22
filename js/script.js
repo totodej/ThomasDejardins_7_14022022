6f8b2b70bfd24d137cea576eb409a469c099a13d
Thomas Dejardins <tdejardins@gmail.com>
2022-02-22 18:46:07 +0100

    feat(script): add functions to show the lists

--- js/script.js
+++ js/script.js
@@ -3,6 +3,18 @@ import { recipes } from "./recipes.js";
 const ingredientsArray = [];
 const appliancesArray = [];
 const ustensilsArray = [];
+const ingredientsList = document.getElementById("ingredients-list");
+const appliancesList = document.getElementById("appliances-list");
+const ustensilsList = document.getElementById("ustensils-list");
+const chevronIngredients = document.getElementById("chevron-ingredients");
+const chevronAppliances = document.getElementById("chevron-appliances");
+const chevronUstensils = document.getElementById("chevron-ustensils")
+const inputIngredients = document.getElementById("input-ingredients");
+const inputAppliances = document.getElementById("input-appliances");
+const inputUstensils = document.getElementById("input-ustensils");
+const recipeButton = document.getElementById("recipe-button");
+const tags = document.getElementById("tags");
+const tagsArray = [];
 
 /////////////////////////////////////////////////////////////
 
@@ -61,9 +73,85 @@ function getRecipe(){
     }
 }
 
+/////////////////////////////////////////////////////////////
+
+function displayIngredientsList(array){
+    array.forEach(function(ingredient, index){
+        ingredientsList.innerHTML += `<li><a href="#" id="ingredient-${index}">${ingredient}</a></li>`;
+    });
+}
+
+function displayAppliancesList(array){
+    array.forEach(function(appliance, index){
+        appliancesList.innerHTML += `<li><a href="#" id="appliance-${index}">${appliance}</a></li>`;
+    })
+}
+
+function displayUstensilsList(array){
+    array.forEach(function(ustensil, index){
+        ustensilsList.innerHTML += `<li><a href="#" id="ustensil-${index}">${ustensil}</a></li>`;
+    });
+}
+
+/////////////////////////////////////////////////////////////
+
+function showDropdown(element){
+    element.style.display = "grid";
+}
+
+function hideDropdown(element){
+    element.style.display = "none";
+}
+
+/////////////////////////////////////////////////////////////
 
 function init(){
     getRecipe();
+    displayIngredientsList(ingredientsArray);
+    displayAppliancesList(appliancesArray);
+    displayUstensilsList(ustensilsArray);
+
+    chevronIngredients.addEventListener("click", function(){
+        if(ingredientsList.style.display === "grid"){
+            hideDropdown(ingredientsList);
+            inputIngredients.style.width = "140px";
+        } else {
+            showDropdown(ingredientsList);
+            hideDropdown(appliancesList);
+            hideDropdown(ustensilsList);
+            inputIngredients.style.width = "566px";
+            inputAppliances.style.width = "140px";
+            inputUstensils.style.width = "140px";
+        }
+    });
+    
+    chevronAppliances.addEventListener("click", function(){
+        if(appliancesList.style.display === "grid"){
+            hideDropdown(appliancesList);
+            inputAppliances.style.width = "140px";
+        } else {
+            showDropdown(appliancesList);
+            hideDropdown(ingredientsList);
+            hideDropdown(ustensilsList);
+            inputAppliances.style.width = "566px";
+            inputIngredients.style.width = "140px";
+            inputUstensils.style.width = "140px";
+        }
+    });
+    
+    chevronUstensils.addEventListener("click", function(){
+        if(ustensilsList.style.display === "grid"){
+            hideDropdown(ustensilsList);
+            inputUstensils.style.width = "140px";
+        } else {
+            showDropdown(ustensilsList);
+            hideDropdown(ingredientsList);
+            hideDropdown(appliancesList);
+            inputUstensils.style.width = "566px";
+            inputIngredients.style.width = "140px";
+            inputAppliances.style.width = "140px";
+        }
+    })
 }
 
 init();
